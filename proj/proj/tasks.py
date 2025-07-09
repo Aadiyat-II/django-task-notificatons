1b9e35d9338328abaeb5daf4a8343d78653aca0d
Aadiyat Ahmad <aadiyat.ahmad@outlook.com>
2025-07-09 16:46:58 -0400

    format output

--- proj/proj/tasks.py
+++ proj/proj/tasks.py
@@ -8,13 +8,13 @@ channel_layer = get_channel_layer()
 @shared_task
 def long_running_add(group_name, x, y):
     time.sleep(5)
-    print(x + y)
+    print(f"Sum: {x + y}")
     async_to_sync(channel_layer.group_send)(group_name, {"type": "task.complete"})
 
 @shared_task
 def long_running_mult(group_name, x, y):
     time.sleep(5)
-    print(x * y)
+    print(f"Product: {x * y}")
     async_to_sync(channel_layer.group_send)(group_name, {"type": "task.complete"})
 
 TASKS = {
