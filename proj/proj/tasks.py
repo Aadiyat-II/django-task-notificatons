395e3fad31be4089df13956c0e0a73fb6f1bbb9e
Aadiyat-II <aadiyat.ahmad@outlook.com>
2025-07-09 16:47:27 -0400

    multiple tasks (#4)
    * map task names to callables
    
    * format output

--- proj/proj/tasks.py
+++ proj/proj/tasks.py
@@ -10,3 +10,14 @@ def long_running_add(group_name, x, y):
     time.sleep(5)
     print(f"Sum: {x + y}")
     async_to_sync(channel_layer.group_send)(group_name, {"type": "task.complete"})
+
+@shared_task
+def long_running_mult(group_name, x, y):
+    time.sleep(5)
+    print(f"Product: {x * y}")
+    async_to_sync(channel_layer.group_send)(group_name, {"type": "task.complete"})
+
+TASKS = {
+    "add" : long_running_add,
+    "mult" : long_running_mult,
+}
