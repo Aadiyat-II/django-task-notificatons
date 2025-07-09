e421a19951214d32f237e0b9f8c1c343be3afff5
Aadiyat-II <aadiyat.ahmad@outlook.com>
2025-07-09 16:22:04 -0400

    notify websocket instance when task ends through channel layers (#3)

--- proj/proj/tasks.py
+++ proj/proj/tasks.py
@@ -1,7 +1,12 @@
 import time
 from celery import shared_task
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+channel_layer = get_channel_layer()
 
 @shared_task
-def long_running_add(x, y):
+def long_running_add(group_name, x, y):
     time.sleep(5)
     print(f"Sum: {x + y}")
+    async_to_sync(channel_layer.group_send)(group_name, {"type": "task.complete"})
